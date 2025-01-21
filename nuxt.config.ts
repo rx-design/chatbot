34f8ac2ba4227ec995ef2bca030d1153de0f6f06
Pjotr Surkov <pjotr@live.com>
2025-01-21 13:29:02 +0200

    Add ESLint, Tailwind and PrimeVue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,32 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxtjs/tailwindcss',
+    '@primevue/nuxt-module',
+    '@vueuse/nuxt',
+  ],
+  devtools: {
+    enabled: true,
+  },
+  css: [
+    '~/assets/css/main.css',
+  ],
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  eslint: {
+    config: {
+      stylistic: true,
+    },
+  },
+  primevue: {
+    options: {
+      theme: 'none',
+    },
+  },
+  tailwindcss: {
+    viewer: false,
+  },
 })
---
+++ tailwind.config.ts
@@ -0,0 +1,8 @@
+import type { Config } from 'tailwindcss'
+import primeUi from 'tailwindcss-primeui'
+
+export default {
+  plugins: [
+    primeUi,
+  ],
+} satisfies Partial<Config>
